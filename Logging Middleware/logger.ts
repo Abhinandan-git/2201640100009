2c40e58a43c83be43cbead614ffa0056f5c60085
Abhinandan Jain <nandanjain04@gmail.com>
2025-09-08 14:58:27 +0530

    Logging complete

--- Logging Middleware/logger.ts
+++ Logging Middleware/logger.ts
@@ -1,3 +1,8 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
 interface Information {
 	stack: "backend" | "frontend";
 	level: "debug" | "info"| "warn" | "error" | "fatal";
@@ -5,17 +10,14 @@ interface Information {
 	message: string;
 }
 
-const Log = async (stack: Information["stack"], level: Information["level"], package: Information["packages"], message: Information["message"]): Promise<void> => {
+const Log = async (stack: Information["stack"], level: Information["level"], pkg: Information["packages"], message: Information["message"]): Promise<void> => {
 	await fetch("http://20.244.56.144/evaluation-service/logs", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify({
-			"stack": stack,
-			"level": level,
-			"package": package,
-			"message": message
-		}),
+		body: JSON.stringify({ stack, level, package: pkg, message }),
 	});
 }
+
+app.listen(4000, () => console.log("TS Logger running on port 4000"));
