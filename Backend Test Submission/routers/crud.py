a0441b59fb0a1d6b5dcf28e45319c52eb512e440
Abhinandan Jain <nandanjain04@gmail.com>
2025-09-08 14:21:45 +0530

    URL creation complete

--- Backend Test Submission/routers/crud.py
+++ Backend Test Submission/routers/crud.py
@@ -1,7 +1,28 @@
 from fastapi import APIRouter
+from datetime import datetime, timedelta
+
+from db.db import database, save_db
 
 router = APIRouter()
 
 @router.post("/shorturls")
-def generate_short_url():
-	pass
+def generate_short_url(url: str, shortcode: str = "", validity: int = 30):
+    expiry_date = datetime.now() + timedelta(minutes=validity)
+
+    # auto-generate shortcode if empty
+    if shortcode == "":
+        shortcode = str(int(datetime.now().timestamp()))[-6:]
+
+    if shortcode not in database:
+        database[shortcode] = {
+            "validity": expiry_date.isoformat(),
+            "url": url,
+            "code_hit": 0,
+            "creation_date": datetime.now().isoformat()
+        }
+        save_db()
+
+    return {
+        "shortLink": f"/{shortcode}",
+        "expiry": expiry_date
+    }
